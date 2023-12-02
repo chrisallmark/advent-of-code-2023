da4ebf668028402c82590bbd035ede06ed9fda9c
Chris Allmark <chris.allmark@vodafone.com>
2023-12-02 17:12:45 +0000

    .

---
+++ src/solutions/day/day.test.ts
@@ -0,0 +1,19 @@
+const day = require("./index.ts");
+
+const input = day.loadInput("day.txt");
+
+describe("Day 1", () => {
+  test("Load Input", () => {
+    expect(input).toEqual("");
+  });
+
+  test("Crunch Input", () => {
+    expect(day.crunchInput(input)).toEqual([0, 0]);
+  });
+
+  test("Solve", () => {
+    expect(day.crunchInput(input)).toEqual([0, 0]);
+  });
+});
+
+export {};
