feaeeae8ff3cd7286e9c2f9285d6ae7972ec3c60
Chris Allmark <chris.allmark@vodafone.com>
2023-12-04 10:44:17 +0000

    .

--- src/solutions/day03/index.ts
+++ src/solutions/day03/index.ts
@@ -5,84 +5,80 @@ const loadInput = (filename: string) => {
 };
 
 const crunchInput = (input: string) => {
-  interface SchematicData {
+  interface Part {
     id: string;
-    type: string;
     x: number;
     y: number;
+  }
+  interface Symbol {
+    id: string;
     gear?: Array<number>;
     ratio?: number;
+    x: number;
+    y: number;
   }
-  const schematic: Array<SchematicData> = [];
-  input.split("\n").map((line, index) => {
+  const parts: Array<Part> = [];
+  const symbols: Array<Symbol> = [];
+  input.split("\n").forEach((line, index) => {
     let id = "";
     for (let i = 0; i < line.length; i++) {
       if (/\d/.test(line[i])) {
         id += line[i];
       } else {
         if (id.length > 0) {
-          schematic.push({ id, type: "part", x: i - id.length, y: index });
+          parts.push({ id, x: i - id.length, y: index });
         }
         if (line[i] !== ".") {
-          schematic.push({
+          symbols.push({
             id: line[i],
-            type: "symbol",
-            x: i,
-            y: index,
             gear: [],
             ratio: 0,
+            x: i,
+            y: index,
           });
         }
         id = "";
       }
     }
     if (id.length > 0) {
-      schematic.push({
+      parts.push({
         id,
-        type: "part",
         x: line.length - id.length,
         y: index,
       });
     }
   });
-  const partOne = schematic
-    .filter((value) => value.type === "part")
-    .reduce((result, part) => {
-      let adjacent = false;
-      for (let y = -1; y <= 1; y++) {
-        for (let x = -1; x <= part.id.length; x++) {
-          adjacent =
-            schematic.filter((symbol) => {
-              adjacent =
-                symbol.type === "symbol" &&
-                part.x + x === symbol.x &&
-                part.y + y === symbol.y;
-              if (symbol.id === "*" && adjacent) {
-                symbol.gear!.push(Number.parseInt(part.id));
-                symbol.ratio = symbol.gear!.reduce(
-                  (total, value) => total * value,
-                  1
-                );
-              }
-              return adjacent;
-            }).length > 0;
-          if (adjacent) break;
-        }
+  const partOne = parts.reduce((result, part) => {
+    let adjacent = false;
+    for (let y = -1; y <= 1; y++) {
+      for (let x = -1; x <= part.id.length; x++) {
+        adjacent =
+          symbols.filter((symbol) => {
+            adjacent = part.x + x === symbol.x && part.y + y === symbol.y;
+            if (symbol.id === "*" && adjacent) {
+              symbol.gear!.push(Number.parseInt(part.id));
+              symbol.ratio = symbol.gear!.reduce(
+                (total, value) => total * value,
+                1
+              );
+            }
+            return adjacent;
+          }).length > 0;
         if (adjacent) break;
       }
-      if (adjacent) {
-        result += Number.parseInt(part.id);
-      }
-      return result;
-    }, 0);
-  const partTwo = schematic
-    .filter((symbol) => symbol.type === "symbol")
-    .reduce((total, symbol) => {
-      if (symbol.gear!.length > 1) {
-        total += symbol.ratio || 0;
-      }
-      return total;
-    }, 0);
+      if (adjacent) break;
+    }
+    if (adjacent) {
+      result += Number.parseInt(part.id);
+    }
+    return result;
+  }, 0);
+  const partTwo = symbols.reduce((total, symbol) => {
+    if (symbol.gear!.length > 1) {
+      total += symbol.ratio ?? 0;
+    }
+    return total;
+  }, 0);
   return [partOne, partTwo];
 };
 
