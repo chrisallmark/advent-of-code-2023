985ffc0653ad20057a26efb5bb93d527676d58b8
Chris Allmark <chris.allmark@vodafone.com>
2023-12-08 07:36:23 +0000

    .

--- src/solutions/day07/day07.test.ts
+++ src/solutions/day07/day07.test.ts
@@ -2,17 +2,19 @@ const day = require("./index.ts");
 
 const input = day.loadInput("day07.txt");
 
-describe("Day 1", () => {
+describe("Day 7", () => {
   test("Load Input", () => {
-    expect(input).toEqual("");
+    expect(input).toEqual(
+      "32T3K 765\nT55J5 684\nKK677 28\nKTJJT 220\nQQQJA 483"
+    );
   });
 
   test("Crunch Input", () => {
-    expect(day.crunchInput(input)).toEqual([0, 0]);
+    expect(day.crunchInput(input)).toEqual([6440, 5905]);
   });
 
   test("Solve", () => {
-    expect(day.crunchInput(input)).toEqual([0, 0]);
+    expect(day.crunchInput(input)).toEqual([6440, 5905]);
   });
 });
 
--- src/solutions/day07/index.ts
+++ src/solutions/day07/index.ts
@@ -4,8 +4,86 @@ const loadInput = (filename: string) => {
   return readFileSync(`${__dirname}/${filename}`, "utf-8");
 };
 
+const handrank = (hand: string, joker: boolean = false) =>
+  hand
+    .split("")
+    .map((card) => (joker ? "J23456789T-QKA" : "-23456789TJQKA").indexOf(card));
+
+interface CardMap {
+  [key: string]: any;
+}
+const cardmap = (hand: Array<string>) =>
+  hand.reduce((cardmap, card) => {
+    cardmap[card] = cardmap[card] + 1 || 1;
+    return cardmap;
+  }, {} as CardMap);
+
+const handtype = (hand: string) => {
+  const result = cardmap(hand.split(""));
+  switch (Object.values(result).sort().join(",")) {
+    case "5":
+      return 7;
+    case "1,4":
+      return 6;
+    case "2,3":
+      return 5;
+    case "1,1,3":
+      return 4;
+    case "1,2,2":
+      return 3;
+    case "1,1,1,2":
+      return 2;
+    default:
+      return 1;
+  }
+};
+
+const wildcard = (hand: string) => {
+  let besthand = hand;
+  let besthandtype = 0;
+  "AKQT98765432".split("").forEach((card) => {
+    const wildhand = hand.replace(/J/g, card);
+    const wildhandtype = handtype(wildhand);
+    if (wildhandtype > besthandtype) {
+      besthand = wildhand;
+      besthandtype = wildhandtype;
+    }
+  });
+  return besthand;
+};
+
+const calculate = (input: string, joker: boolean = false) =>
+  input
+    .split("\n")
+    .map((line) => {
+      const [hand, bid] = line.split(" ");
+      return { hand, bid };
+    })
+    .sort((a, b) => {
+      let handtypeA = joker ? handtype(wildcard(a.hand)) : handtype(a.hand);
+      let handtypeB = joker ? handtype(wildcard(b.hand)) : handtype(b.hand);
+      if (handtypeA === handtypeB) {
+        const handrankA = handrank(a.hand, joker);
+        const handrankB = handrank(b.hand, joker);
+        for (let i = 0; i < 5; i++) {
+          if (handrankA[i] > handrankB[i]) {
+            return 1;
+          }
+          if (handrankA[i] < handrankB[i]) {
+            return -1;
+          }
+        }
+        return 0;
+      }
+      return handtypeA - handtypeB;
+    })
+    .reduce((result, data, index) => {
+      result += Number.parseInt(data.bid) * (index + 1);
+      return result;
+    }, 0);
+
 const crunchInput = (input: string) => {
-  return [0, 0];
+  return [calculate(input), calculate(input, true)];
 };
 
 module.exports = {
