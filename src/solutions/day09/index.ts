a768d10078b1425885471ab98a147aa100da1009
Chris Allmark <chris.allmark@vodafone.com>
2023-12-09 16:10:00 +0000

    .

--- src/solutions/day09/index.ts
+++ src/solutions/day09/index.ts
@@ -4,10 +4,7 @@ const loadInput = (filename: string) => {
   return readFileSync(`${__dirname}/${filename}`, "utf-8");
 };
 
-const reducer = (
-  data: Array<number>,
-  reverse: boolean = false
-): Array<number> => {
+const reducer = (data: Array<number>): Array<number> => {
   const reduced = data.reduce((result, value, index) => {
     if (index + 1 < data.length) {
       result.push(data[index + 1] - value);
@@ -15,14 +12,10 @@ const reducer = (
     return result;
   }, [] as Array<number>);
   if (reduced.reduce((result, value) => result && value === 0, true)) {
-    reverse ? reduced.unshift(0) : reduced.push(0);
+    reduced.push(0);
   } else {
-    const recursed = reducer(reduced, reverse);
-    reverse
-      ? reduced.unshift(reduced[0] - recursed[0])
-      : reduced.push(
-          reduced[reduced.length - 1] + recursed[recursed.length - 1]
-        );
+    const recursed = reducer(reduced);
+    reduced.push(reduced[reduced.length - 1] + recursed[recursed.length - 1]);
   }
   return reduced;
 };
@@ -35,8 +28,8 @@ const crunchInput = (input: string) => {
   }, 0);
   const partTwo = input.split("\n").reduce((result, line) => {
     const data = line.split(" ").map((value) => Number.parseInt(value));
-    const reduced = reducer(data, true);
-    return result + (data[0] - reduced[0]);
+    const reduced = reducer(data.reverse());
+    return result + data[data.length - 1] + reduced[reduced.length - 1];
   }, 0);
   return [partOne, partTwo];
 };
